c2151c8ec8bac8f8fa490a2d731d9ae45a720155
Kyrylo <kyrylo.mm@gmail.com>
2023-10-21 02:27:46 +0200

    First task already done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+let lightbox;
+const gallery = document.querySelector('.gallery');
+const galleryTemplate = galleryItems.map(({ preview, original, description }) => {
+  return `<li class='gallery__item'>
+   <a class='gallery__link' href='${original}'>
+      <img class='gallery__image' src='${preview}' alt='${description}' />
+   </a>
+</li>`;
+}).join('');
+gallery.insertAdjacentHTML('beforeend', galleryTemplate);
+lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    fadeSpeed: 250,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    close: true,
+    closeText: '×',
+    nav: true,
+    navText: ['←', '→'],
+    download: 'Click to download button',
+  });
